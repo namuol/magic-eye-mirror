47e99211da3f74b5e51c142b9bc4fb89aebde22a
Louis Acresti <louis.acresti@gmail.com>
2025-06-08 11:50:41 -0700

    set up compute texture

--- src/main.ts
+++ src/main.ts
@@ -2,41 +2,22 @@ import Stats from 'stats.js';
 import * as t from 'three/tsl';
 import * as THREE from 'three/webgpu';
 
-class App {
-  threejs_: THREE.WebGPURenderer | undefined;
-  scene_: THREE.Scene | undefined;
-  camera_: THREE.Camera | undefined;
-  stats_: Stats;
-
+class Level {
+  camera: THREE.PerspectiveCamera;
+  scene: THREE.Scene;
   bricks: THREE.Mesh[] = [];
 
   constructor() {
-    this.stats_ = new Stats();
-  }
-
-  async initialize() {
-    this.threejs_ = new THREE.WebGPURenderer();
-    document.body.appendChild(this.threejs_.domElement);
-    document.body.appendChild(this.stats_.dom);
-
-    window.addEventListener(
-      'resize',
-      () => {
-        this.onWindowResize_();
-      },
-      false,
-    );
-
-    this.scene_ = new THREE.Scene();
-    this.scene_.background = new THREE.Color('#000');
-
-    this.camera_ = new THREE.PerspectiveCamera(
+    this.camera = new THREE.PerspectiveCamera(
       75, // fov
       window.innerWidth / window.innerHeight, // aspect
       0.001, // near
       4, // far
     );
-    this.camera_.position.set(0, 0, 1);
+    this.camera.position.set(0, 0, 1);
+
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color('#000');
 
     // Setup objects:
     {
@@ -89,81 +70,131 @@ class App {
             top - brickHeight / 2 - y * (brickHeight + gap),
             -2,
           );
-          this.scene_.add(box);
+          this.scene.add(box);
           this.bricks.push(box);
         }
       }
     }
+  }
 
-    // Setup compute texture:
-    {
-      const width = window.innerWidth;
-      const height = window.innerHeight;
-      const storageTexture = new THREE.StorageTexture(width, height);
-      const computeTexture = t.Fn(
-        ({storageTexture}: {storageTexture: THREE.StorageTexture}) => {
-          const posX = t.instanceIndex.mod(width);
-          const posY = t.instanceIndex.div(width);
-          const indexUV = t.uvec2(posX, posY);
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+  }
+}
 
-          // https://www.shadertoy.com/view/Xst3zN
+class Autostereogram {
+  camera: THREE.OrthographicCamera;
+  scene: THREE.Scene;
+  computeNode: THREE.ComputeNode;
 
-          const x = t.float(posX).div(50.0);
-          const y = t.float(posY).div(50.0);
+  constructor() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-          const v1 = x.sin();
-          const v2 = y.sin();
-          const v3 = x.add(y).sin();
-          const v4 = x.mul(x).add(y.mul(y)).sqrt().add(5.0).sin();
-          const v = v1.add(v2, v3, v4);
+    const aspect = width / height;
 
-          const r = v.sin();
-          const g = v.add(Math.PI).sin();
-          const b = v.add(Math.PI).sub(0.5).sin();
+    this.camera = new THREE.OrthographicCamera(-aspect, aspect, 1, -1, 0, 2);
+    this.camera.position.z = 1;
+    this.scene = new THREE.Scene();
 
-          t.textureStore(
-            storageTexture,
-            indexUV,
-            t.vec4(r, g, b, 1),
-          ).toWriteOnly();
-        },
-      );
+    const storageTexture = new THREE.StorageTexture(width, height);
 
-      // compute
+    const computeTexture = t.Fn(
+      ({storageTexture}: {storageTexture: THREE.Texture}) => {
+        const posX = t.instanceIndex.mod(width);
+        const posY = t.instanceIndex.div(width);
+        const indexUV = t.uvec2(posX, posY);
 
-      const computeNode = computeTexture({storageTexture}).compute(
-        width * height,
-      );
+        // https://www.shadertoy.com/view/Xst3zN
 
-      // compute texture
-      this.threejs_.computeAsync(computeNode);
+        const x = t.float(posX).div(50.0);
+        const y = t.float(posY).div(50.0);
 
-      const material = new THREE.MeshBasicNodeMaterial({color: 0x00ff00});
-      material.colorNode = t.texture(storageTexture);
+        const v1 = x.sin();
+        const v2 = y.sin();
+        const v3 = x.add(y).sin();
+        const v4 = x.mul(x).add(y.mul(y)).sqrt().add(5.0).sin();
+        const v = v1.add(v2, v3, v4);
 
-      const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
-      this.scene_.add(plane);
-    }
+        const r = v.sin();
+        const g = v.add(Math.PI).sin();
+        const b = v.add(Math.PI).sub(0.5).sin();
 
+        t.textureStore(
+          storageTexture,
+          indexUV,
+          t.vec4(r, g, b, 1),
+        ).toWriteOnly();
+      },
+    );
+
+    this.computeNode = computeTexture({storageTexture}).compute(width * height);
+
+    const material = new THREE.MeshBasicNodeMaterial({color: 0x00ff00});
+    material.colorNode = t.texture(storageTexture);
+
+    const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+    this.scene.add(plane);
+  }
+
+  onWindowResize() {
+    const aspect = window.innerWidth / window.innerHeight;
+
+    const frustumHeight = this.camera.top - this.camera.bottom;
+
+    this.camera.left = (-frustumHeight * aspect) / 2;
+    this.camera.right = (frustumHeight * aspect) / 2;
+
+    this.camera.updateProjectionMatrix();
+  }
+}
+
+class App {
+  renderer: THREE.WebGPURenderer;
+  level: Level;
+  autostereogram: Autostereogram;
+  stats: Stats;
+
+  constructor() {
+    this.stats = new Stats();
+    this.renderer = new THREE.WebGPURenderer();
+    document.body.appendChild(this.renderer.domElement);
+    document.body.appendChild(this.stats.dom);
+
+    window.addEventListener(
+      'resize',
+      () => {
+        this.onWindowResize_();
+      },
+      false,
+    );
+
+    this.level = new Level();
+    this.autostereogram = new Autostereogram();
+  }
+
+  async run() {
     this.onWindowResize_();
     this.raf_();
   }
 
   onWindowResize_() {
-    this.threejs_?.setSize(window.innerWidth, window.innerHeight);
-    if (this.camera_ instanceof THREE.PerspectiveCamera) {
-      this.camera_.aspect = window.innerWidth / window.innerHeight;
-      this.camera_.updateProjectionMatrix();
-    }
+    this.renderer?.setSize(window.innerWidth, window.innerHeight);
+    this.level.onWindowResize();
+    this.autostereogram.onWindowResize();
   }
 
   raf_() {
     requestAnimationFrame(async () => {
-      if (!this.threejs_ || !this.scene_ || !this.camera_) return;
-
-      this.stats_.begin();
-      await this.threejs_.renderAsync(this.scene_, this.camera_);
-      this.stats_.end();
+      this.stats.begin();
+      await this.renderer.renderAsync(this.level.scene, this.level.camera);
+      await this.renderer.computeAsync(this.autostereogram.computeNode);
+      await this.renderer.renderAsync(
+        this.autostereogram.scene,
+        this.autostereogram.camera,
+      );
+      this.stats.end();
 
       this.raf_();
     });
@@ -172,5 +203,5 @@ class App {
 
 window.addEventListener('DOMContentLoaded', async () => {
   const app = new App();
-  await app.initialize();
+  await app.run();
 });
