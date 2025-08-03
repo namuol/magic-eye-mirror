2bc4ae06931fe62a6e21dda3514de75fb2f34b0e
Louis Acresti <louis.acresti@gmail.com>
2025-08-02 17:16:03 -0700

    project rename

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import glsl from 'vite-plugin-glsl';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [glsl()],
-  base: '/magic-eye-yourself/',
+  base: '/magic-eye-mirror/',
 });
